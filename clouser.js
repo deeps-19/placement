a52a89e17fe8506b4fe01ffcc93290c61e8c26c8
deeps-19 <62871080+deeps-19@users.noreply.github.com>
2023-06-21 17:40:16 +0530

    Create clouser.js

---
+++ clouser.js
@@ -0,0 +1,12 @@
+//write a code on closures and explain
+function print()
+{
+    function second()
+    {
+        console.log("inside function");
+    }
+     second();
+   //if function call is outside the fuction then its get error that function is not define
+}
+
+print()
